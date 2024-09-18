c5a8822a7342a4a094455e54edf150aa2f333f40
Foisal Ahmed <shakhfoisal99@gmail.com>
2024-09-18 17:57:13 +0600

    dom event

---
+++ js/todo.js
@@ -0,0 +1,14 @@
+document.getElementById("task-btn").addEventListener("click", function () {
+  const messageContainer = document.getElementById("input-value");
+  const value = messageContainer.value;
+  const container = document.getElementById("content-container");
+  const p = document.createElement("p");
+  p.innerText = value;
+  container.appendChild(p);
+  messageContainer.value = "";
+
+  //   delete all
+  document.getElementById("clear-btn").addEventListener("click", function () {
+    container.removeChild(p)
+  });
+});
