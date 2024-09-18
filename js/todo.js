722cf4066e496266863b117f2682203e4b22cd70
Foisal Ahmed <shakhfoisal99@gmail.com>
2024-09-18 18:12:32 +0600

    almost

--- js/todo.js
+++ js/todo.js
@@ -9,6 +9,19 @@ document.getElementById("task-btn").addEventListener("click", function () {
 
   //   delete all
   document.getElementById("clear-btn").addEventListener("click", function () {
-    container.removeChild(p)
+    container.removeChild(p);
   });
+});
+
+// keyboard event
+
+const messageContainer = document.getElementById("input-value");
+messageContainer.addEventListener("keyup", function (event) {
+  if (event.key === "Enter") {
+    const container = document.getElementById("content-container");
+    const p = document.createElement("p");
+    p.innerText = messageContainer.value;
+    container.appendChild(p);
+    messageContainer.value = "";
+  }
 });
